743c824ad467b72866fac687ae3a7ca8c6ed68cd
unknown <yogeshmore044@gmail.com>
2023-01-24 12:52:54 +0530

    initial

---
+++ intro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 1</title>
+</head>
+
+<body>
+
+
+    <?php
+
+    for ($i = 0; $i <= 10; $i++) {
+        echo "<input type='text' placeholder='$i'> <br/>";
+    }
+
+    ?>
+
+    <?php
+    //$a = null;
+    /* $a = '';
+    echo "Hello World";
+    echo "Hello World2";
+    ?>
+
+    <?php
+    if (1 == 10) {
+
+    ?>
+        <h1>Hello I am true</h1>
+    <?php
+    }*/
+    ?>
+</body>
+
+</html>
